8ca0b58292db0a7648de3e8972443d34a5838899
Madushani <mmweerasekara@gmail.com>
2023-02-15 00:53:47 +0530

    Update simple_cal.py
    Divide, Multiply, Clear , Exit inside the while loop

--- simple_cal.py
+++ simple_cal.py
@@ -1,21 +1,52 @@
-print("1. Add")
-print("2. Subtract")
-print("3. Divide")
-print("4. Multiply")
-print("5. Clear")
-print("6. Exit")
 
-option = input("Select an operation and press enter: ")
+While True:
+    
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Divide")
+    print("4. Multiply")
+    print("5. Clear")
+    print("6. Exit")
 
-if int(option) == 1:
-    num1 = int(input("Enter num1: "))
-    num2 = int(input("Enter num2: "))
-    sum = num1 + num2
-    print(sum)
+    option = input("Select an operation and press enter: ")
 
-elif int(option) == 2:
-    num1 = int(input("Enter num1: "))
-    num2 = int(input("Enter num2: "))
-    sub = num1 - num2
-    print(sub)
+    if int(option) == 1:
+    
+        num1 = int(input("Enter num1: "))
+        num2 = int(input("Enter num2: "))
+        sum = num1 + num2
+        print(sum)
 
+    elif int(option) == 2:
+    
+        num1 = int(input("Enter num1: "))
+        num2 = int(input("Enter num2: "))
+        sub = num1 - num2
+        print(sub)
+
+    elif int(option) == 3:
+    
+        num1 = int(input("Enter num1: "))
+        num2 = int(input("Enter num2: "))
+        divide = num1 / num2
+        print(divide)
+
+    elif int(option) == 4:
+    
+        num1 = int(input("Enter num1: "))
+        num2 = int(input("Enter num2: "))
+        multiply = num1 * num2
+        print(multiply)
+
+    elif int(option) == 5:
+    
+        print("Calculator cleared")
+
+    elif int(option) == 5:
+    
+        print("Existing calculator")
+        break
+
+    else:
+        print("Invalid operation,enter a number between 1 and 6.")
+    
