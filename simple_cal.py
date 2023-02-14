08b6df08a9735adf4df0c614df9a3014a1341f9d
Madushani <mmweerasekara@gmail.com>
2023-02-15 00:20:37 +0530

    Update simple_cal.py
    add function

--- simple_cal.py
+++ simple_cal.py
@@ -1,6 +1,14 @@
 print("1. Add")
-print("2. Subsract")
-print("3. Devide")
+print("2. Subtract")
+print("3. Divide")
 print("4. Multiply")
+print("5. Clear")
+print("6. Exit")
 
-option = input("Select an operation and press enter : " )
+option = input("Select an operation and press enter: ")
+
+if int(option) == 1:
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    sum = num1 + num2
+    print(sum)
